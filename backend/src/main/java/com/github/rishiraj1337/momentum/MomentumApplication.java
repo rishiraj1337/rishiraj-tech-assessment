11c36c58db354861b1b716e4073bc82d98bcdfe5
rishiraj1337 <rishiraj1337@users.noreply.github.com>
2026-08-19 11:14:21 +0530

    Initialize momentum monorepo

---
+++ backend/src/main/java/com/github/rishiraj1337/momentum/MomentumApplication.java
@@ -0,0 +1,13 @@
+package com.github.rishiraj1337.momentum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentumApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/rishiraj1337/momentum/MomentumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rishiraj1337.momentum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
